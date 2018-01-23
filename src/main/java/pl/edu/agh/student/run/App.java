6c190336ecfd4fec7869c58ef4198f5b131aae32
Kamil Chlebek <kamil.chlebek94@gmail.com>
2018-01-23 19:51:57 +0100

    it's alive!

--- src/main/java/pl/edu/agh/student/run/App.java
+++ src/main/java/pl/edu/agh/student/run/App.java
@@ -4,8 +4,8 @@ import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCompute;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import pl.edu.agh.student.model.GameState;
 import pl.edu.agh.student.TicTacToeTask;
+import pl.edu.agh.student.model.GameState;
 
 public class App {
 
@@ -17,13 +17,23 @@ public class App {
         clientConfig.setIgniteInstanceName("client");
         clientConfig.setClientMode(true);
 
+        // ======= Configuration for remote node discovery
+//        TcpDiscoverySpi spi = new TcpDiscoverySpi();
+//        TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
+//        ipFinder.setAddresses(Collections.singletonList("108.48.19.91"));
+//        spi.setIpFinder(ipFinder);
+//        clientConfig.setDiscoverySpi(spi);
+
         // ======= Start client
         Ignite client = Ignition.start(clientConfig);
         IgniteCompute compute = client.compute();
 
-        // ======= Run task
-        // Todo: generate random (or load from file) initial game state
-        GameState initialGameState = new GameState(new int[]{}, 0, 1);
+        // ======= Initial state
+        int gameSize = 3;
+        int[] initialState = new int[gameSize*gameSize];
+        GameState initialGameState = new GameState(initialState, initialState.length, 1);
+
+        // ======= Run
         Double cnt = compute.execute(TicTacToeTask.class, initialGameState);
 
         System.out.println("Result: " + cnt);
